dad1b2998a37e9a299821b4b6c55094fcc1d22f6
Gabriel Kloster Morais <gklostermorais@gmail.com>
2026-04-13 15:21:45 -0300

    Aula sobre react

---
+++ aula 8/projeto/src/components/contador/index.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+export default function Contador() {
+    return (
+        <>
+            <h1 className='contador'>Contador</h1>
+            <p>0</p>
+        </> 
+    )
+}

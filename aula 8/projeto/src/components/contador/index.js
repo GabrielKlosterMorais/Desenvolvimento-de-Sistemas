f04867426efd2875aa2b88abe6f33f8b0c57f46b
Gabriel Kloster Morais <gklostermorais@gmail.com>
2026-04-13 17:08:20 -0300

    Adição de atividade incompleta

--- aula 8/projeto/src/components/contador/index.js
+++ aula 8/projeto/src/components/contador/index.js
@@ -1,10 +1,35 @@
 import './style.css'
+import {    useState    } from 'react';
 
 export default function Contador() {
+    // const [num, setNum] = useState(0);
+    const [resultado, setResultado] = useState("Falso");
+
+    function verificar(valor) {
+        if(valor>10){
+            setResultado("Verdadeiro")
+        } else {
+            setResultado("Falso")
+        }
+    }
+    // function incrementar() {
+    //     setNum(num + 1)
+    // }
+
+    // function descrementar() {
+    //     setNum(num - 1)
+    // }
+
     return (
-        <>
-            <h1 className='contador'>Contador</h1>
-            <p>0</p>
-        </> 
+        <div className='pagina'>
+            {/* <h1>Contador</h1> */}
+            <h3>É maior que 10?</h3>
+            <input type='text' onChange={(e) => verificar(e.target.value)}></input> 
+            <p>{resultado}</p>
+
+            {/* <button onClick={incrementar}>+</button>
+            <p>{num}</p>
+            <button onClick={descrementar}>-</button>  */}
+        </div> 
     )
 }
